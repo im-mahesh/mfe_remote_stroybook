21f8aba081fa8bedee0b12df393fc6c76f236037
mahesh_sv <memaheshwaran@gmail.com>
2024-04-27 19:53:17 +0530

    Resuable component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Button from "reusable_npm/dist/Button";
+import Input from "reusable_npm/dist/Input";
 
 function App() {
-  return <div className="App">haii</div>;
+  return (
+    <div className="App" style={{ width: "50%" }}>
+      <Button />
+      <Input />
+    </div>
+  );
 }
 
 export default App;
